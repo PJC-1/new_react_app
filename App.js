edb25bf6bd6dc75d556b180f0749733d27255097
PJC-1 <jumpzine@gmail.com>
2016-10-24 22:50:23 -0700

    commented out first class component to try stateless function component

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-class App extends React.Component {
-  render(){
-    return <div>Hi foo</div>
-  }
-}
+// class App extends React.Component {
+//   render(){
+//     return <div>Hi foo</div>
+//   }
+// }
+
+// stateless function component,
+// constant variable called App, that is equal to a
+// function that returns our JSX.
+const App = () => <h1>This also works</h1>
 
 export default App

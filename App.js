44a5b8aba26dffa0d4bbb4c4e7500ef2a710980d
PJC-1 <jumpzine@gmail.com>
2016-10-26 13:52:52 -0700

    learning about refs

--- App.js
+++ App.js
@@ -1,51 +1,64 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     //super provides the context for 'this' within our component
     super();
-    //we can now set the state within an object, this is basically like the defualts
-    //you can access the values of the state is by interpolating it like the this.props
-    this.state = { txt: '' }
+
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     //you can cache here, so that you don't have to write out bind(this) later on.
     this.update = this.update.bind(this)
   }
   //were using an update to manage our txt's state
   //our custom method takes an event as an argument
   update(e){
-    //this.setState is how we set a new state
-    //e.target.value is the value that we are going to pass into the key 'txt'
-    this.setState({txt: e.target.value})
-  }
-  //in the render we have parentheses and a div that wrap the whole thing
-  //so that we only have one node.
-  //we created an input field, and to trigger the update we add a 'onChange'
-  //event, and call this.update, and bind that to this.
-  //note that the 'this' in the render is the same 'this' in context of our App component
 
-  //the App component is rendering the Widget component, so App is the parent and
-  //Widget is the child component.
-  //owner-ownee relationship, the parent component is also known as the composite component
-  //using JSX we set the props txt to this.state.txt and the update to this.update
-  //this is updating the state.txt of the parent component
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
+  }
+
   render(){
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br />
       </div>
     );
   }
 }
 
-//we are creating another component, a stateless component that takes props and returns
-//the input field and h1
-//the child component should only take props.
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  );
+class Slider extends React.Component {
+  render(){
+    return (
+        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
+    );
+  }
 }
+
+// //we are creating another component, a stateless component that takes props and returns
+// //the input field and h1
+// //the child component should only take props.
+// const Widget = (props) => {
+//   return (
+//     <div>
+//       <input type="text" onChange={props.update} />
+//       <h1>{props.txt}</h1>
+//     </div>
+//   );
+// }
 
 export default App

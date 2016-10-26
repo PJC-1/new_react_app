8bbd61820783632c12c209e84745aa5cd0f1d775
PJC-1 <jumpzine@gmail.com>
2016-10-26 08:17:46 -0700

    created state for App component and update custom method

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
+  constructor(){
+    //super provides the context for 'this' within our component
+    super();
+    //we can now set the state within an object, this is basically like the defualts
+    //you can access the values of the state is by interpolating it like the this.props
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  //were using an update to manage our txt's state
+  //our custom method takes an event as an argument
+  update(e){
+    //this.setState is how we set a new state
+    //e.target.value is the value that we are going to pass into the key 'txt'
+    this.setState({txt: e.target.value})
+  }
   render(){
-
-    return <h1>Hello world</h1>
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
   }
 }
 

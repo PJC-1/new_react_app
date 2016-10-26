7d31f9da664b04ebd9d42bafd07eda268491f819
PJC-1 <jumpzine@gmail.com>
2016-10-26 07:56:44 -0700

    went back to having the render function in the main.js

--- App.js
+++ App.js
@@ -2,31 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
   render(){
-    let txt = this.props.txt
-    return <div>{txt}</div>
+
+    return <h1>Hello world</h1>
   }
 }
 
-// stateless function component,
-// constant variable called App, that is equal to a
-// function that returns our JSX.
-// const App = () => <h1>This also works</h1>
-
-// export default App
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  //isRequired will need to be specified in the component
-  cat: React.PropTypes.number.isRequired
-}
-
-//this is where you can declare default props which will
-//be rendered if something else is not specified in the App component
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
-ReactDOM.render(
-  <App cat={5} txt="this is the props value" />,
-  document.getElementById('app')
-);
+export default App

9ea6db49812b867d763f6b8d3ee6b1a44fba0513
PJC-1 <jumpzine@gmail.com>
2016-10-28 22:23:45 -0700

    utilizing componentWillReceiveProps shouldComponentUpdate and componentDidUpdate

--- App.js
+++ App.js
@@ -2,109 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
-
     super();
-
-    this.state = {
-      val: 0,
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-
     this.update = this.update.bind(this)
-    this.updateTwo = this.updateTwo.bind(this)
-  }
-
-  updateTwo(e){
-    this.setState({val: this.state.val +1 })
-  }
-
-  componentWillMount(){
-    this.setState({m: 2})
+    this.state = {increasing: false}
   }
 
   update(e){
-
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
+    ReactDOM.render(<App val={this.props.val+1} />, document.getElementById('app'));
   }
 
-  // here we are going to return a another component named Button.
-  render(){
-    console.log('rendering!')
-    return (
-      <div>
-        <button onClick={this.updateTwo}>{this.state.val * this.state.m}</button>
-        <Button> I <Heart/> tacos </Button>
-        <br />
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
-      </div>
-    );
+  componentWillReceiveProps(nextProps){
+    this.setState({increasing: nextProps.val > this.props.val})
   }
 
-  // here wer are declaring inc (for increment), and set interval to update
-  //every half second
-  componentDidMount(){
-    this.inc = setInterval(this.updateTwo,500)
-  }
-
-  //we need to clear the interval when we unmount because if we don't will
-  //cause an error that says were are calling setState on a component that
-  //been unmounted.
-  componentWillUnmount(){
-    clearInterval(this.inc)
-  }
-}
-
-class Wrapper extends React.Component {
-  constructor(){
-    super();
-  }
-  mount(){
-    ReactDOM.render(<App />, document.getElementById('a'))
-  }
-  unmount(){
-    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0;
   }
   render(){
+    console.log(this.state.increasing)
     return (
-      <div>
-        <button onClick={this.mount.bind(this)}>Mount</button>
-        <button onClick={this.unmount.bind(this)}>Unmount</button>
-        <div id="a"></div>
-      </div>
+      <button onClick={this.update}>{this.props.val}</button>
     )
   }
-}
-
-class Button extends React.Component {
-  render(){
-    return <button>{this.props.children}</button>
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps)
   }
 }
 
-class Slider extends React.Component {
-  render(){
-    return (
-      <div>
-        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
-      </div>
-    );
-  }
-}
+App.defaultProps = { val: 0 }
 
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
-
-export default Wrapper
+export default App

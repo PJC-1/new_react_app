bcecde9507546b04f81eafbb9b4f368e966f465c
PJC-1 <jumpzine@gmail.com>
2016-10-29 21:56:43 -0700

    set up mixin

--- App.js
+++ App.js
@@ -1,4 +1,29 @@
 import React from 'react';
+
+let Mixin = InnerComponent => class extends React.Component {
+  constructor(){
+    super();
+    this.update = this.update.bind(this)
+    this.state = {val: 0}
+  }
+  update(e){
+    this.setState({val: this.state.val + 1})
+  }
+  componentWillMount(){
+    console.log('will mount')
+  }
+  render(){
+    return <InnerComponent update={this.update} {...this.state} {...this.props} />
+  }
+  componentDidMount(){
+    console.log('mounted')
+  }
+}
+
+const Button = (props) => <button onClick={props.update}>{props.txt} - {props.val}</button>
+
+let ButtonMixed = Mixin(Button)
+
 class App extends React.Component {
   constructor(){
     super();
@@ -13,7 +38,9 @@ class App extends React.Component {
   }
   render(){
     return (
-      <button onClick={this.update}>{this.props.txt} - {this.state.val}</button>
+      <div>
+        <ButtonMixed txt="Button" />
+      </div>
     );
   }
   componentDidMount(){

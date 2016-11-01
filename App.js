26f69cc643e625c2449ea533be45146fb1db4b3b
PJC-1 <jumpzine@gmail.com>
2016-10-31 21:59:32 -0700

    added propTypes defaultProps to make more reusable components

--- App.js
+++ App.js
@@ -8,8 +8,6 @@ class App extends React.Component {
     this.state = {
       val: 0,
       red: 0,
-      green: 0,
-      blue: 0
     }
 
     this.update = this.update.bind(this)
@@ -25,11 +23,8 @@ class App extends React.Component {
   }
 
   update(e){
-
     this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
     })
   }
 
@@ -41,14 +36,7 @@ class App extends React.Component {
         <button onClick={this.updateTwo}>{this.state.val * this.state.m}</button>
         <Button> I <Heart/> tacos </Button>
         <br />
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
+        <NumInput ref="red" min={0} max={255} step={1} val={+this.state.red} label="Red" update={this.update} />
         <br />
       </div>
     );
@@ -95,16 +83,36 @@ class Button extends React.Component {
   }
 }
 
-class Slider extends React.Component {
+class NumInput extends React.Component {
   render(){
+    let label = this.props.label !== '' ? <label>{this.props.label} - {this.props.val}</label> : ''
     return (
       <div>
-        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
+        <input ref="inp" type={this.props.type} min={this.props.min} max={this.props.max} step={this.props.step} onChange={this.props.update} />
+        {label}
       </div>
     );
   }
 }
 
+NumInput.propTypes = {
+  min: React.PropTypes.number,
+  max: React.PropTypes.number,
+  step: React.PropTypes.number,
+  val: React.PropTypes.number,
+  label: React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: '',
+  type: 'range'
+}
 const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 export default Wrapper
